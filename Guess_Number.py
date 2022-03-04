ffd4dfc54d6b0aa5219bea5dae1b93c63266da1c
Amit Jadhav <amitjadhav270800@gmail.com>
2022-03-05 04:06:41 +0530

    Initial commit

---
+++ Guess_Number.py
@@ -0,0 +1,25 @@
+# Guess the number
+"""
+Number of guesses - 9
+print no of guesses left
+no of guesses he took to finish
+game over
+"""
+import random
+
+n = random.randrange(100)
+count = 9
+while(1):
+    m = int(input("Guess the number from 1 - 100\n"))
+    count -= 1
+    if n > m:
+        print("Increase the number")
+    elif m > n:
+        print("Reduce the number")
+    else:
+        print("You guessed the number right")
+        print('You took', 9-count, 'guesses to guess the number')
+        print('Game over')
+        break
+    print('You are left with', count, 'guesses')
+    print()
